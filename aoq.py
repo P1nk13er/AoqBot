723fa0a83b696da8a0cb033644860424ba61b042
huangjunxin <huang-junxin@qq.com>
2022-09-30 17:23:17 +0800

    Add Aoq 自序

--- aoq.py
+++ aoq.py
@@ -1,10 +1,10 @@
 import random
 
-aoq = ["xs", "还行", "有毒", "?", "qs", "艹", "笑死", "确实", "？"]
+aoq = ["xs", "笑死", "还行", "有毒", "?", "？", "qs", "确实", "草", "艹"]
 while(True):
-    n = input("你问aoq:")
+    n = input("你问aoq：")
     num = random.randint(0, len(aoq))
     if num == len(aoq):
-        print("aoq没有回复你" + '\n')
+        print("aoq没有回复你。" + '\n')
     else:
-        print("aoq回复了：" + aoq[num] + '\n')
+        print("aoq：" + aoq[num] + '\n')
